e2dac9cb24be2869552467bfccfde46072cfdbc1
SyedaAnabia <samdrive30@gmail.com>
2025-12-29 04:58:31 +0500

    Update env.js

--- static/env.js
+++ static/env.js
@@ -1,1 +1,1 @@
-window.CLERK_PUBLISHABLE_KEY = 'pk_test_Z3JhdGVmdWwtbmFyd2hhbC02My5jbGVyay5hY2NvdW50cy5kZXYk'
+window.CLERK_PUBLISHABLE_KEY ='pk_test_Z3JhdGVmdWwtbmFyd2hhbC02My5jbGVyay5hY2NvdW50cy5kZXYk'
